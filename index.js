223751073c9bf6861a3816449c2024ab69f724f1
Robin Bron <finwo@pm.me>
2019-10-21 22:59:19 +0200

    Secret keys can now be generated

--- index.js
+++ index.js
@@ -1,83 +1,6 @@
-const b64    = require('base64url');
-const BigInt = require('big-integer');
-
-function rand(bits) {
-  return BigInt(Buffer.from(Array(bits/8).fill(0).map(a=>Math.floor(Math.random()*256))).toString('hex'), 16);
-}
-
-function randPrime(bits, minbits) {
-  let n = BigInt(0);
-  let min = minbits ? BigInt(2).pow(minbits) : 0;
-  while(n.lesser(min) || (!n.isPrime())) {
-    n = rand(bits);
-  }
-  return n;
-}
-
-function primefactors(n, testlimit) {
-  let factors = [];
-  let i = BigInt(3);
-
-  while(n.mod(2).isZero()) {
-    factors.push(2);
-    n = n.over(2);
-    if (n.isPrime()) {
-      return factors;
-    }
-  }
-
-  // Fully factor it
-  for(i=i; i.times(i).lt(n); i = i.plus(2)) {
-    if(i.gt(testlimit)) return false;
-    while(n.mod(i).isZero()) {
-      factors.push(i);
-      n = n.over(i);
-      if (n.isPrime()) {
-        return factors;
-      }
-    }
-  }
-
-  return factors
-}
-
-function buildGenerator(p, testlimit) {
-  let r       = BigInt(2);
-  let phi     = p.minus(1);
-  let factors = primefactors(phi, testlimit);
-
-  if (false === factors) return false;
-
-  while(r.lesserOrEquals(p)) {
-    let found = false;
-
-    for(const factor of factors) {
-      if (!r.modPow(phi.over(factor), p).compare(1)) {
-        found = true;
-        break;
-      }
-    }
-
-    if (!found) {
-      return r;
-    }
-
-    r = r.plus(1);
-  }
-
-  return false;
-}
-
-function genPub(bits, complexity) {
-  complexity = BigInt(2).pow(complexity || 24);
-  let n = false;
-  let g = false;
-  do {
-    n = randPrime(bits, bits-8);
-    g = buildGenerator(n, complexity);
-  } while(g === false);
-  return [n,g];
-}
+const b64     = require('base64url');
+const BigInt  = require('big-integer');
+const KeyPair = require('./keypair');
 
 function Hashnet(opt) {
 
@@ -86,13 +9,8 @@ function Hashnet(opt) {
     bootstrap: [],
   }, opt);
 
-  let [n,g] = genPub(opt.bits);
-  this.n = n;
-  this.g = g;
+  this.kp = new KeyPair();
 
-  do {
-    this.a = rand(opt.bits);
-  } while(this.a.gt(this.n));
 
   // this.p  = 
   // this.sk = Buffer.from(Array(32).fill(0).map(Math.floor(Math.random()*256)));
@@ -112,4 +30,5 @@ function Hashnet(opt) {
 //   console.log(sender);
 // }
 
-module.exports = Hashnet;
+Hashnet.KeyPair = KeyPair;
+module.exports  = Hashnet;
--- test.js
+++ test.js
@@ -1,9 +1,35 @@
 const Hashnet = require('./index');
 
-const net = new Hashnet();
+let Alice = new Hashnet.KeyPair();
+console.log('Alice', Alice);
 
-console.log(net);
+let Bob   = new Hashnet.KeyPair();
+console.log('Bob'  , Bob);
 
+// Key alice sends with
+let AliceTx = new Hashnet.KeyPair({
+  n: Bob.n,
+  s: Alice.s
+});
+console.log('AliceTx', AliceTx);
 
-// console.log([n,g]);
+let AliceTxSecret = AliceTx.sharedSecret(Bob);
+console.log('AliceTxSecret', AliceTxSecret);
+
+let BobRxSecret = Bob.sharedSecret(AliceTx);
+console.log('BobRxSecret', BobRxSecret);
+
+// Key bob sends with
+let BobTx = new Hashnet.KeyPair({
+  n: Alice.n,
+  s: Bob.s
+});
+console.log('BobTx', BobTx);
+
+let BobTxSecret = BobTx.sharedSecret(Alice);
+console.log('BobTxSecret', BobTxSecret);
+
+let AliceRxSecret = Alice.sharedSecret(BobTx);
+console.log('AliceRxSecret', AliceRxSecret);
+
 // console.log('g =', buildGenerator(n));
